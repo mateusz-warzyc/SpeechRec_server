5b9a54b39a2c006fa6bcba17976345c7ae01d622
mateusz-warzyc <mateuszw19911@gmail.com>
2017-06-17 16:59:31 +0200

    initial spring boot structure

---
+++ src/main/java/pl/mateuszwarzyc/sr/config/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pl.mateuszwarzyc.sr.config;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpeechRecServerApplication.class);
+    }
+
+}
---
+++ src/main/java/pl/mateuszwarzyc/sr/config/SpeechRecServerApplication.java
@@ -0,0 +1,12 @@
+package pl.mateuszwarzyc.sr.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeechRecServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeechRecServerApplication.class, args);
+    }
+}
